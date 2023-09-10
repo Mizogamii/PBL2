8720768a0de9d71dd54aed98b8918aacc41682be
Sayumi <mizogami18@gmail.com>
2023-09-10 12:09:51 -0300

    Create jogo2048.py

---
+++ jogo2048.py
@@ -0,0 +1,32 @@
+print("----------------------------------------------------------------------")
+print("\t\t\t\t2048")
+print("----------------------------------------------------------------------")
+print("INSTRUÇÕES DO JOGO")
+print("----------------------------------------------------------------------")
+print("Digite:")
+print("""W para mover para cima
+S para mover para baixo
+A para mover para esquerda
+D para mover para direita""")
+print("----------------------------------------------------------------------")
+
+matriz = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+for i in range(0,4):
+    for j in range(0,4):
+        matriz[i][j] = "0"
+
+print("+---+---+---+---+")
+for i in range(0,4):
+    for j in range(0,4):
+       print(f"| {matriz[i][j]} ", end="")
+    print("|\n+---+---+---+---+")
+
+movimentos = input("Informe o comando [W, S, A, D]: ")
+movimentos = movimentos.upper()
+
+while movimentos != "W" and movimentos != "S" and movimentos != "A" and movimentos != "D":
+    movimentos = input("Informe o comando [W, S, A, D]: ")
+    movimentos = movimentos.upper()
+
+print(f"{movimentos}")
