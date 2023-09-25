44d57d9490b4e9e49a334ce8d18f659eb2cb54b7
Sayumi <mizogami18@gmail.com>
2023-09-25 12:03:22 -0300

    Update testandoFunção.py

--- testandoFunção.py
+++ testandoFunção.py
@@ -178,6 +178,22 @@ while continuar != "N" and continuar != "n":
     numeros = [2,4]
     ganhou = False
     aindaTemChance = True
+    matriz[0][0] = 2
+    matriz[0][1] = 4
+    matriz[0][2] = 4
+    matriz[0][3] = 2
+    matriz[1][0] = 2
+    matriz[1][1] = 2 
+    matriz[1][2] = 4
+    matriz[1][3] = 2
+    matriz[2][0] = 4
+    matriz[2][1] = 8
+    matriz[2][2] = 4
+    matriz[2][3] = 4
+    matriz[3][0] = 4
+    matriz[3][1] = 8
+    matriz[3][2] = 16
+    matriz[3][3] = 32
   
     while ganhou != True or aindaTemChance != True:  
         
@@ -210,16 +226,6 @@ while continuar != "N" and continuar != "n":
                     matriz[linha][coluna] = numeroSorteado          
                     
         contador += 1    
-        matriz[0][0] = 
-        matriz[0][1] =
-        matriz[0][2] =
-        matriz[0][3] =
-        matriz[1][0] =
-        matriz[1][1] =
-        matriz[1][2] =
-        matriz[1][3] =
-
-               
                                     
         print("+-----------------------------------+")
         for i in range(0,4):
