9c0499574cca24a16c7d73a0186acc644fe8a0c5
Sayumi <mizogami18@gmail.com>
2023-10-01 14:48:08 -0300

    3

--- coresTeste.py
+++ coresTeste.py
@@ -5,7 +5,7 @@ matriz[0][1] = 512
 matriz[0][2] = 64
 matriz[0][3] = 128
 matriz[1][0] = 2
-matriz[1][1] = 2 
+matriz[1][1] = 0 
 matriz[1][2] = 4
 matriz[1][3] = 2
 matriz[2][0] = 4
@@ -21,27 +21,12 @@ print("+-----------------------------------+")
 for i in range(0,4):
     print(f"|        |        |        |        |")
     for j in range(0,4):
-        if matriz[i][j] < 9:
-            if matriz[i][j] == 2:
-                print("|   \033[91m{}\033[0m   ".format(matriz[i][j]), end=' ')
-            elif matriz[i][j] == 4:
-                print("|   \033[92m{}\033[0m   ".format(matriz[i][j]), end=' ')
-            elif matriz[i][j] == 8:
-                print("|   \033[93m{}\033[0m   ".format(matriz[i][j]), end=' ')
-            
+        if matriz[i][j] == 0:
+            print(f"|   \033[0;33m{matriz[i][j]}\033[m    ", end="")
+        elif matriz[i][j] < 9:
+            print(f"|   {matriz[i][j]}    ", end="")
         elif matriz[i][j] < 99:
-            if matriz[i][j] == 16:    
-                print("|   \033[94m{}\033[0m  ".format(matriz[i][j]), end=' ')
-            elif matriz[i][j] == 32:
-                print("|   \033[95m{}\033[0m  ".format(matriz[i][j]), end=' ')
-            elif matriz[i][j] == 64:
-                print("|   \033[96m{}\033[0m  ".format(matriz[i][j]), end=' ')
-            elif matriz[i][j] == 128:
-                print("|\033[95m{}\033[0m   ".format(matriz[i][j]), end='')
-            elif matriz[i][j] == 256:
-                print("|\033[94m{}\033[0m   ".format(matriz[i][j]), end='')
-            elif matriz[i][j] == 512:
-                print("|\033[93m{}\033[0m   ".format(matriz[i][j]), end='')
+            print(f"|   {matriz[i][j]}   ", end="")
             
         elif matriz[i][j] < 999:
             print(f"|   {matriz[i][j]}  ", end="")
@@ -51,4 +36,3 @@ for i in range(0,4):
             
     print(f"|\n|        |        |        |        |")
     print("+-----------------------------------+")
-        
