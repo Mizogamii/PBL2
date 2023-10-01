b5c506b8a593b01ae62718f1ed6f34a5153d8517
Sayumi <mizogami18@gmail.com>
2023-10-01 18:08:36 -0300

    Update testePyGame.py

--- testePyGame.py
+++ testePyGame.py
@@ -11,7 +11,11 @@ altura = 480
 tela = pygame.display.set_mode((largura, altura))
 
 while True:
-    for 
-
+    for event in pygame.event.get():
+        if event.type == quit:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+    
 
 
