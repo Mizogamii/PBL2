107a57d59a8f12ddb72f3907f4900de610fc4e9b
Mizogamii <120470907+Mizogamii@users.noreply.github.com>
2023-10-30 15:49:27 -0300

    a

---
+++ import pygame.py
@@ -0,0 +1,5 @@
+import pygame
+pygame.init()
+
+# Listar todas as fontes disponíveis
+print(pygame.font.get_fonts(), end='\n')
--- testePyGame.py
+++ testePyGame.py
@@ -14,6 +14,9 @@ y = altura / 2
 x_azul = randint(40,600)
 y_azul = randint(50,430)
 
+pontos = 0
+fonte = pygame.font.SysFont("arial", 40, True, True)
+
 tela = pygame.display.set_mode((largura, altura))
 
 pygame.display.set_caption("Jogo 2048")
@@ -23,6 +26,8 @@ relogio = pygame.time.Clock()
 while True:
     relogio.tick(30)
     tela.fill((0,0,0))
+    mensagem = f'Pontos: {pontos}'
+    textoFormatado = fonte.render(mensagem, True, (255,255,255))
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -60,7 +65,9 @@ while True:
     if ret_vermelho.colliderect(ret_azul):
         x_azul = randint(40,600)
         y_azul = randint(50,430)
+        pontos += 1
 
+    tela.blit(textoFormatado, (450,40))
     """if y >= altura:
         y = 0
     y = y + 5"""
