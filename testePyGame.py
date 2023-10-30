6057556ff95b3e3aada5c39929b656f3e3304415
Mizogamii <120470907+Mizogamii@users.noreply.github.com>
2023-10-30 14:07:52 -0300

    Update testePyGame.py

--- testePyGame.py
+++ testePyGame.py
@@ -1,14 +1,18 @@
 import pygame
 from pygame.locals import *
-
 from sys import exit
+from random import randint
 
 pygame.init()
 
 largura = 640
 altura = 480
+
 x = largura / 2
 y = altura / 2
+
+x_azul = randint(40,600)
+y_azul = randint(50,430)
 
 tela = pygame.display.set_mode((largura, altura))
 
@@ -19,14 +23,43 @@ relogio = pygame.time.Clock()
 while True:
     relogio.tick(30)
     tela.fill((0,0,0))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+        """#Nesse formato ele só anda de um em um: 
         if event.type == KEYDOWN:
             if event.key == K_a:
                 x = x - 20
-    pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
+            if event.key == K_d:
+                x = x + 20
+            if event.key == K_w:
+                y = y - 20
+            if event.key == K_s:
+                y = y + 20"""
+        
+    #Já dessa forma se continuar apertando ele anda    
+    if pygame.key.get_pressed()[K_a]:
+        x = x - 20
+    
+    if pygame.key.get_pressed()[K_d]:
+        x = x + 20
+    
+    if pygame.key.get_pressed()[K_w]:
+        y = y - 20
+
+    if pygame.key.get_pressed()[K_s]:
+        y = y + 20
+
+    ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
+    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul,y_azul,40,50))
+
+    #Aqui o colliderect verifica se um "sobrepós" o outro, nesse caso vai verificar se o vermelho sobrepós o azul
+    if ret_vermelho.colliderect(ret_azul):
+        x_azul = randint(40,600)
+        y_azul = randint(50,430)
 
     """if y >= altura:
         y = 0
